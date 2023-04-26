471e4a0b7b996d85d559a471adbf3824616863a4
Breno Cadette <breno.cadette@gmail.com>
2023-04-26 20:51:32 -0300

    second commit

---
+++ reactapp/src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+
+import './style.css';
+
+export function Home() {
+
+  return (
+    <div className='container'> 
+      <h1>List Of Attendance</h1>
+
+      <input type="text" placeholder="Write a name..." />
+      <button type="button">Add</button>
+    </div>
+    )
+}
+
