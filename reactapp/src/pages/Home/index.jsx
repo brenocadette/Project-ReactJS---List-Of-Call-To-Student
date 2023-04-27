8eb71058c00354ca6fb2757f1405abefa2150394
Breno Cadette <breno.cadette@gmail.com>
2023-04-26 21:40:26 -0300

    4º commit - initializing useState

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -1,16 +1,37 @@
+import React, {useState} from 'react';// é um hook que permite que nós criemos um estado 
+
 import './style.css';
 import {Card} from '../../componentes/Card/index'
 
 export function Home() {
+ /*
+   let studentName = "";
+
+function handleNameChange (name){
+  console.log(name);
+  studentName=name;
+
+}*/
+
+const [studentName, setStudentName] = useState("Student");
 
   return (
     <div className='container'> 
-      <h1>List Of Attendance</h1>
+      <h1>List Of Attendance </h1>
+      <h1>Name: {studentName}</h1>
 
-      <input type="text" placeholder="Write a name..." />
+      <input 
+      type="text" 
+      placeholder="Write a name..." 
+      //onChange={mudancaValue => handleNameChange(mudancaValue.target.value)}
+      onChange={mudancaValue => setStudentName(mudancaValue.target.value)}
+
+      />
+
       <button type="button">Add</button>
 
-      <Card />
+      <Card name="Breno" time="10:10:10" id="01"/>
+      <Card name="Ana" time="11:11:11" id="02"/>
     </div>
     )
 }
